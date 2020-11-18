7f7bcb23e976c5d4decf6d7c0e6009df8f345d61
Nemo Xiong <xiongnemo@126.com>
2020-11-19 03:24:32 +0800

    package specification: test: fix typos

--- src/test/java/specification/SpecificationTest.java
+++ src/test/java/specification/SpecificationTest.java
@@ -22,15 +22,15 @@ public class SpecificationTest {
         facilityList.add(new MadHatterTeaCups());
         System.out.println("All facilities:");
         facilityList.forEach((facility) -> System.out.println(facility.getName() + " " + facility.getFacilityType() + " " + facility.getFacilitySuitableAge() + " " + facility.getFacilityThrillLevel()));
-        System.out.println("Sorting out facilities which is OUTDOOR:");
+        System.out.println("Sorting out facilities which is OUTDOOR...");
         List<Facility> outdoorFacilityList = facilityList.stream().filter(new FacilityTypeSelector(FacilityType.OUTDOOR)).collect(Collectors.toList());
         System.out.println("OUTDOOR facilities:");
         outdoorFacilityList.forEach((facility) -> System.out.println(facility.getName() + " " + facility.getFacilityType()));
-        System.out.println("Sorting out facilities which is SCARY:");
+        System.out.println("Sorting out facilities which is SCARY...");
         List<Facility> scaryFacilityList = facilityList.stream().filter(new FacilityThrillLevelSelector(FacilityThrillLevel.SCARY)).collect(Collectors.toList());
         System.out.println("SCARY facilities:");
         scaryFacilityList.forEach((facility) -> System.out.println(facility.getName() + " " + facility.getFacilityThrillLevel()));
-        System.out.println("Sorting out facilities which is OUTDOOR and suitable for KIDS:");
+        System.out.println("Sorting out facilities which is OUTDOOR and suitable for KIDS...");
         List<Facility> ourdoorTweenFacilityList = facilityList.stream().filter(new FacilityTypeSelector(FacilityType.OUTDOOR)).filter(new FacilitySuitableAgeSelector(FacilitySuitableAge.KIDS)).collect(Collectors.toList());
         System.out.println("OUTDOOR and suitable for KIDS facilities:");
         ourdoorTweenFacilityList.forEach((facility) -> System.out.println(facility.getName() + " " + facility.getFacilityType() + " " + facility.getFacilitySuitableAge()));
