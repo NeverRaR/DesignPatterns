9f0d4c92c20d8f7583c29ad26c43e13393207658
Never <897013703@qq.com>
2020-11-19 01:01:13 +0800

    update unit test

--- src/test/java/activeobject/ActiveObjectTest.java
+++ src/test/java/activeobject/ActiveObjectTest.java
@@ -1,7 +1,21 @@
 package activeobject;
 
+import org.junit.Test;
+
 public class ActiveObjectTest {
-    public static void main(String[] args) {
-        new SolveEventThread("顾客",ActiveObjectFactory.createActiveObject(),200).start();
+    @Test
+    public  void main() {
+
+        try {
+            NotifyThread notifyThread=new NotifyThread("Notification",200);
+            SolveEventThread solveEventThread =
+                    new SolveEventThread("Customer", ActiveObjectFactory.createActiveObject(),notifyThread, 200);
+            solveEventThread.start();
+            solveEventThread.join();
+            notifyThread.join();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
