fcdfee7a1b2f4a04d2578927920e2b83d5e54ef0
Never <897013703@qq.com>
2020-11-19 21:31:23 +0800

    unit test fixed

--- src/main/java/activeobject/SchedulerThread.java
+++ src/main/java/activeobject/SchedulerThread.java
@@ -15,7 +15,7 @@ public class SchedulerThread extends Thread {
     public void setDead(){//主动对象停止接受请求
         if(isDead()) return;
         this.dead=true;
-        queue.putRequest(new NullMethodRequest());//放入空请求唤醒进程
+        queue.putRequest(new NullMethodRequest());//放入空请求唤醒线程
     }
     public void run(){
         while(true){
