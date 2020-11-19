6b098f444428324cf38413742b2514d0772cef0c
Nntraveler <nntraveler@foxmail.com>
2020-11-19 20:44:30 +0800

    Finish observer

---
+++ src/test/java/observer/ObserverTest.java
@@ -0,0 +1,21 @@
+package observer;
+
+import org.junit.Test;
+
+public class ObserverTest {
+    @Test
+    public void main(){
+        // create observable peopleNum
+        PeopleNum peopleNum = new PeopleNum();
+        // create observers
+        NumScreen numScreen = new NumScreen(peopleNum);
+        EntranceEmployee entranceEmployee = new EntranceEmployee(peopleNum);
+        // update peopleNum and observers
+        peopleNum.setVal(80);
+        peopleNum.setVal(105);
+        peopleNum.deleteObserver(entranceEmployee);
+        EntranceEmployee entranceEmployee2 = new EntranceEmployee(peopleNum, 200);
+        peopleNum.setVal(90);
+        peopleNum.setVal(120);
+    }
+}
