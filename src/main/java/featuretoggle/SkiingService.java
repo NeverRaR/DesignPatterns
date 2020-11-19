8ab89406a4452c3336c5997d13cf4e7585ed0296
badcode666 <rui2000666@gmail.com>
2020-11-19 16:05:06 +0800

    add Feature Toggle Mode

---
+++ src/main/java/featuretoggle/SkiingService.java
@@ -0,0 +1,10 @@
+package featuretoggle;
+
+import featuretoggle.player.Player;
+
+public interface SkiingService {
+    /**
+     * @param player 传入要进行化学的玩家
+     */
+    void showPlayerGroupMsg(Player player);
+}
