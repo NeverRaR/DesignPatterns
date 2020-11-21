1d22601b2f98dea583420fee2ee3a3c6e250c6d1
Nemo Xiong <xiongnemo@126.com>
2020-11-22 02:04:51 +0800

    add comments, style codes for templatemethod's test

--- src/test/java/templatemethod/TemplateMethodTest.java
+++ src/test/java/templatemethod/TemplateMethodTest.java
@@ -1,22 +1,31 @@
 package templatemethod;
 
-import java.util.List;
 import java.util.ArrayList;
-import org.junit.Test;
+import java.util.List;
 import java.util.stream.Collectors;
-
+import org.junit.Test;
 import templatemethod.*;
 
+/**
+ * @author nemo
+ */
 public class TemplateMethodTest {
+
     @Test
     public void main() {
         System.out.println("Test template method:");
         System.out.println("Creating cabinOne with DiningCabin..");
-        HalflingCabin cabinOne = new HalflingCabin("cabinOne", new DiningCabin());
+        HalflingCabin cabinOne = new HalflingCabin(
+            "cabinOne",
+            new DiningCabin()
+        );
         System.out.println("Letting Nemo enter..");
         cabinOne.visitedBy("Nemo");
         System.out.println("Creating cabinTwo with SaloonCabin..");
-        HalflingCabin cabinTwo = new HalflingCabin("cabinTwo", new SaloonCabin());
+        HalflingCabin cabinTwo = new HalflingCabin(
+            "cabinTwo",
+            new SaloonCabin()
+        );
         System.out.println("Letting Marshmallow enter..");
         cabinTwo.visitedBy("Marshmallow");
         System.out.println("Changing cabinOne's cabin type to HardSeatCabin..");
