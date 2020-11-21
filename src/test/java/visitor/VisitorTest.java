48c0ab5afcd41505ea076912fec416d06f0f2f47
Nemo Xiong <xiongnemo@126.com>
2020-11-22 02:05:34 +0800

    add comments, style codes for visitor's test

--- src/test/java/visitor/VisitorTest.java
+++ src/test/java/visitor/VisitorTest.java
@@ -1,21 +1,34 @@
 package visitor;
 
-import org.junit.Test;
-
-import visitor.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
+import org.junit.Test;
+import visitor.*;
 
+/**
+ * @author nemo
+ */
 public class VisitorTest {
+
     @Test
     public void main() {
         System.out.println("Test visitor:");
         System.out.println("Initializing a tree structure...");
-        Manager manager = new Manager("Su",
-                new ViceManager("cxf", new Staff("Nemo"), new Staff("Zen"), new Staff("Bulldozer")),
-                new ViceManager("xzc", new Staff("Pussy"), new Staff("Apollo"), new Staff("Ice"))
+        Manager manager = new Manager(
+            "Su",
+            new ViceManager(
+                "cxf",
+                new Staff("Nemo"),
+                new Staff("Zen"),
+                new Staff("Bulldozer")
+            ),
+            new ViceManager(
+                "xzc",
+                new Staff("Pussy"),
+                new Staff("Apollo"),
+                new Staff("Ice")
+            )
         );
         System.out.println("Find all staffs:");
         manager.accept(new StaffVisitor());
@@ -23,6 +36,5 @@ public class VisitorTest {
         manager.accept(new ViceManagerVisitor());
         System.out.println("Find all manager(s):");
         manager.accept(new ManagerVisitor());
-
     }
 }
