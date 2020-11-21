c37777c00a05c3aeb6e3b26ea34b67ea2bbbe0a0
Nemo Xiong <xiongnemo@126.com>
2020-11-22 01:28:11 +0800

    add comments for threadpool

--- src/main/java/threadpool/NavigationTask.java
+++ src/main/java/threadpool/NavigationTask.java
@@ -1,13 +1,22 @@
 package threadpool;
 
+/**
+ * @author nemo
+ */
 public class NavigationTask extends Task {
 
     private static final int TIME_PER_PERSON = 300;
 
+    /**
+     * @param numPeople task related parameter
+     */
     public NavigationTask(Integer numPeople) {
         super(numPeople * TIME_PER_PERSON);
     }
 
+    /**
+     * @return String: task's description
+     */
     @Override
     public String toString() {
         return String.format("%s %s", this.getClass().getSimpleName(), super.toString());
--- src/main/java/threadpool/Worker.java
+++ src/main/java/threadpool/Worker.java
@@ -3,16 +3,23 @@ package threadpool;
 /**
  * Worker implements {@link Runnable} and thus can be executed by {@link
  * java.util.concurrent.ExecutorService}.
+ * @author nemo
  */
 public class Worker implements Runnable {
 
 
     private final Task task;
 
+    /**
+     * @param task task to execute by worker
+     */
     public Worker(final Task task) {
         this.task = task;
     }
 
+    /**
+     * run worker
+     */
     @Override
     public void run() {
         System.out.println(String.format("%s processing %s", Thread.currentThread().getName(), task.toString()));
