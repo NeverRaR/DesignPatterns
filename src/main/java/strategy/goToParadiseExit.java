2ba24eae1ed5a9a10355a929a90cd9ecd0ca395e
LeeJAJA <JJ@hgeek.net>
2020-11-24 01:35:53 +0800

    add comments to strategy

--- src/main/java/strategy/goToParadiseExit.java
+++ src/main/java/strategy/goToParadiseExit.java
@@ -1,6 +1,23 @@
 package strategy;
 
+/**
+ * @Title: goToChillingRestaurant.java
+ *
+ * @Package strategy
+ *
+ * @Description: the implementation of MovingMethod, try to go to Paradise Exit
+ *
+ * @author Jiajie
+ *
+ * @date 2020/11/24
+ */
 public class goToParadiseExit implements MovingMethod{
+
+    /**
+     * @Description: override the specific execute method
+     *
+     * @return: he specific execute method
+     */
     @Override
     public String execute() {
         return "Using the Electric Vehicles to reach the Paradise Exit";
