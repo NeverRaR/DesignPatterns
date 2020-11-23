90027109c9a67bc464922e5612a41234db31ffbf
LeeJAJA <JJ@hgeek.net>
2020-11-24 01:49:45 +0800

    reformat the output of monostate

--- src/test/java/strategy/ExperiencedTouristTest.java
+++ src/test/java/strategy/ExperiencedTouristTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 public class ExperiencedTouristTest {
     @Test
     public void main() {
+        System.out.println("1# Initial Destination");
         ExperiencedTourist Sam = new ExperiencedTourist(new goToChillingRestaurant());
         System.out.print("ExperiencedTourist:getInstance:("+Sam.toString()+"): ");
         System.out.print("Sam: ");
@@ -18,6 +19,7 @@ public class ExperiencedTouristTest {
         System.out.print("Peter: ");
         Peter.goToDestination();
 
+        System.out.println("\n2# Updated Destination");
         Sam.changeMethod(new goToParadiseExit());
         Jack.changeMethod(new goToParadiseExit());
         Peter.changeMethod(new goToParadiseExit());
