c1fe14543bc3292c8261deed9afc2482116710f3
LeeJAJA <JJ@hgeek.net>
2020-11-24 00:43:08 +0800

    add comments to mediator

--- src/main/java/mediator/GroupMember.java
+++ src/main/java/mediator/GroupMember.java
@@ -1,10 +1,35 @@
 package mediator;
-
+/**
+ * @Title: GroupMember.java
+ *
+ * @Package mediator
+ *
+ * @Description: interface GroupMember, defined three basic operations of group member
+ *
+ * @author Jiajie
+ *
+ * @date 2020/11/24
+ */
 public interface GroupMember {
 
+    /**
+     * @Description: let a group member join a group
+     *
+     * @param group: the group to join
+     */
     void joinTourGroup(TourGroup group);
 
+    /**
+     * @Description: Perform an active action
+     *
+     * @param action: the action to perform
+     */
     void groupAction(Action action);
 
+    /**
+     * @Description: Perform a passive action
+     *
+     * @param action: the action to perform
+     */
     void act(Action action);
 }
--- src/main/java/mediator/TourGuides.java
+++ src/main/java/mediator/TourGuides.java
@@ -1,13 +1,34 @@
 package mediator;
 
+/**
+ * @Title: TourGuides.java
+ *
+ * @Package mediator
+ *
+ * @Description: Inherits from GroupMemberBase, the TourGuides class that overrides the toString()
+ *
+ * @author Jiajie
+ *
+ * @date 2020/11/24
+ */
 public class TourGuides extends GroupMemberBase{
 
     private String name;
 
+    /**
+     * @Description: Override toString()
+     *
+     * @return TourGuides(this.name)
+     */
     public TourGuides(String name) {
         this.name = name;
     }
 
+    /**
+     * @Description: Override toString()
+     *
+     * @return TourGuides(this.name)
+     */
     @Override
     public String toString() {
         return String.format("TourGuides(%s)", this.name);
